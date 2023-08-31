35e142fe9b041fc96f4e78ba6f2f702fd9720570
ccampbell04 <cjscampbell@icloud.com>
2023-08-31 21:10:25 +0100

    adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <head>
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6855093518131888'
+          crossOrigin='anonymous'
+        ></script>
+      </head>
       <body>{children}</body>
     </html>
   );
