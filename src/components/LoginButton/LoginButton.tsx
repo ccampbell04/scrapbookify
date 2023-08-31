e4a18e453e2208ffd69d16f8577fe16f63be29e1
ccampbell04 <cjscampbell@icloud.com>
2023-08-31 21:00:19 +0100

    Fix redirecturls

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './LoginButton.module.css';
 
 const authEndpoint = 'https://accounts.spotify.com/authorize';
-const redirectUri = 'http://localhost:3000/';
+const dev_redirectUri = 'http://localhost:3000/';
+const redirectUri = 'https://scrapbookify.vercel.app/';
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 
 const scopes = ['user-top-read'];
+
 
 const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   '%20'
